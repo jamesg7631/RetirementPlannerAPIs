7b329506d367142cca457d841be47dd132e1e5c5
James Glover <james.glover7@outlook.com>
2025-06-29 16:45:09 +0100

    Added Compatibility for other tax regions in UK and armed forces

--- src/models/tax_bands.py
+++ src/models/tax_bands.py
@@ -1,16 +1,24 @@
 from tax_band import TaxBand
 class TaxBands:
 
-    def __init__(self, tax_bands=None):
-        if tax_bands is None:
-            tax_bands = [TaxBand(12570, 20), TaxBand(50270, 40), TaxBand(125140, 45)]
+    def __init__(self, tax_bands=None, residency=None, armed_forces=None):
+        if tax_band == None and residency == None:
+            raise Exception("Tax bands or region must be specified")
+        if ((tax_bands is None and residency == "England") or (tax_bands == None and armed_forces == True) or
+                (tax_bands is None and residency == "Northern_Ireland")) or (tax_bands is None and residency == "Wales"):
+            tax_bands = [TaxBand(12570, 20), TaxBand(50270, 40),
+                         TaxBand(125140, 45)]
+        elif tax_bands is None and residency == "Scotland":
+            tax_bands = [TaxBand(12570, 19), TaxBand(15397, 20)
+                , TaxBand(27491, 21), TaxBand(43662, 42),
+                         TaxBand(75000, 45), TaxBand(125140, 48)]
+
         self.tax_bands = tax_bands
         self.loss_taxable_allowance_threshold = 100000
 
     def yearly_tax_contribution(self, yearly_gross_salary):
         current_amount = yearly_gross_salary
         tax_contribution = 0.0
-        loss_of_taxable_allowance_amount = 100000
         yearly_bands = self.band_adjustment(yearly_gross_salary)
 
         yearly_bands = reversed(yearly_bands)
@@ -42,7 +50,7 @@ class TaxBands:
 
 if __name__ == "__main__":
     tax_band = TaxBands()
-    print(tax_band.yearly_tax_contribution(100001))
+    print(tax_band.yearly_tax_contribution(100010)/ 52)
 
 
 
